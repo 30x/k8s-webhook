2ad88939dad4a152c85b144b4721cd4a9b7f6c7a
Josh Bowen <jbowen@apigee.com>
2016-09-23 15:36:51 -0600

    First pass at chi router

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"net/http"
 
+	"github.com/pressly/chi"
+	"github.com/pressly/chi/middleware"
+)
+
+func main() {
+	flag.Parse()
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root"))
+	})
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	err := http.ListenAndServe(":8081", r)
+	if err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+//TestMain is a hello world test
+func TestMain(*testing.T) {
+	fmt.Printf("Test Hello\n")
+}
